775bad82e3f422dbf8d5b1fc04e3467ee7055d81
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-05-24 17:07:53 +0200

    refactoring code with samuele suggestion

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,23 +1,29 @@
-const posts = require('../data/data');
+const posts = require('../data/data.json');
 
 exports.getPosts = (req, res) => {
-    const accept = req.headers.accept;
-
-    if (accept.includes('application/json')) {
-        res.json(posts);
-    } else if (accept.includes('text/html')) {
-        let html = '<ul>';
-        posts.forEach(post => {
-            html += `<li>
-                        <h2>${post.title}</h2>
-                        <img src="${post.image}" alt="${post.title}">
-                        <p>${post.content}</p>
-                        <p>Tags: ${post.tags.join(', ')}</p>
-                     </li>`;
-        });
-        html += '</ul>';
-        res.send(html);
-    } else {
-        res.status(406).send('Not Acceptable');
-    }
+    
+    res.format({
+        'text/plain': function () {
+            res.send('Plain text response');
+        },
+        'text/html': function () {
+            let html = '<ul>';
+            posts.forEach(post => {
+                html += `<li>
+                            <h2>${post.title}</h2>
+                            <img src="${post.image}" alt="${post.title}">
+                            <p>${post.content}</p>
+                            <p>Tags: ${post.tags.join(', ')}</p>
+                        </li>`;
+            });
+            html += '</ul>';
+            res.send(html);
+        },
+        'application/json': function () {
+            res.json(posts);
+        },
+        default: function () {
+            res.status(406).send('Not Acceptable');
+        }
+    });
 };
