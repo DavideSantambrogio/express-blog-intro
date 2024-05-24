98d29c7207b953d337cce70f5a33bfe08d080938
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-05-24 14:44:20 +0200

    add data array

---
+++ data/data.js
@@ -0,0 +1,35 @@
+// data/data.js
+const posts = [
+    {
+        title: "Post 1",
+        content: "Contenuto del primo post",
+        image: "https://picsum.photos/200/300?random=1",
+        tags: ["tag1", "tag2"]
+    },
+    {
+        title: "Post 2",
+        content: "Contenuto del secondo post",
+        image: "https://picsum.photos/200/300?random=2",
+        tags: ["tag3", "tag4"]
+    },
+    {
+        title: "Post 3",
+        content: "Contenuto del terzo post",
+        image: "https://picsum.photos/200/300?random=3",
+        tags: ["tag5", "tag6"]
+    },
+    {
+        title: "Post 4",
+        content: "Contenuto del quarto post",
+        image: "https://picsum.photos/200/300?random=4",
+        tags: ["tag7", "tag8"]
+    },
+    {
+        title: "Post 5",
+        content: "Contenuto del quinto post",
+        image: "https://picsum.photos/200/300?random=5",
+        tags: ["tag9", "tag10"]
+    }
+];
+
+module.exports = posts;
